d9e0072b6839fadd4b0ddf2bfd2e4f430fcc419c
User <User@SIMONA-PC>
2018-01-26 12:24:22 +0100

    Erstellen eines Fensters

--- UndertaleMonopoly/src/application/Main.java
+++ UndertaleMonopoly/src/application/Main.java
@@ -1,13 +1,18 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		
+		try
+		{
+			FXMLLoader loader = new FXMLLoader();
+		}
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
