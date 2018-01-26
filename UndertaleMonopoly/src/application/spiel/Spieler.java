07379c1b8215cb69d58ad570e0d5b026beca7af6
Kaan <kakirmizi@tsn.at>
2018-01-26 11:38:17 +0100

    g eh emdlich

--- UndertaleMonopoly/src/application/spiel/Spieler.java
+++ UndertaleMonopoly/src/application/spiel/Spieler.java
@@ -1,16 +1,26 @@
 package application.spiel;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class Spieler {
 	private int gold;
-	private Image bild;
+	private ImageView bild;
 	private int position;
 	private Würfel[] würfel = {new Würfel(), new Würfel()}; //Die zwei Würfel
+	
+	public Spieler(){
+		this.gold = 1000;
+		this.position = 0;
+		String url = getClass().getResource("").toString();
+		this.bild = new ImageView(new Image(url, 0, 0, true, false));
+	}
 	
 	public void überweisen(int gold, Spieler s)
 	{
 		//TODO überweisen von Gold an einen anderen Spieler
+		this.gold -= gold;
+		s.setGold(gold+s.getGold());
 	}
 	
 	public void kaufen(Spielfeld sf)
@@ -22,4 +32,18 @@ public class Spieler {
 	{
 		
 	}
+	
+	
+	
+	
+	
+	
+	public int getGold() {
+		return gold;
+	}
+
+	public void setGold(int gold) {
+		this.gold = gold;
+	}
+
 }
