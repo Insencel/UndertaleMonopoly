65ec4967c6563858c61a68f7d124c14b47890e00
Kaan <kakirmizi@tsn.at>
2018-02-02 12:06:17 +0100

    fick die simon

--- UndertaleMonopoly/src/application/spiel/Würfel.java
+++ UndertaleMonopoly/src/application/spiel/Würfel.java
@@ -6,6 +6,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Würfel {
+	private int letzteZahl;
 	private ImageView[] würfelseiten; //hat nur sechs seiten!
 	
 	
@@ -13,8 +14,11 @@ public class Würfel {
 	{
 		 Random randomGenerator = new Random();
 		    int randomInt = randomGenerator.nextInt(6)+1;
+		    letzteZahl = randomInt;
 			return randomInt;
 	}
 	
-	
+	public ImageView getBild(){
+		return würfelseiten[letzteZahl-1];
+	}
 }
