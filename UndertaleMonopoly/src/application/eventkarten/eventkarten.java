f602165bb67efdb53ff231cd964c8f072657a11d
Maste <Maste@T-Desktop>
2018-02-02 12:08:32 +0100

    d

--- UndertaleMonopoly/src/application/eventkarten/eventkarten.java
+++ UndertaleMonopoly/src/application/eventkarten/eventkarten.java
@@ -10,12 +10,13 @@ public class eventkarten {
 	 * Du kaufst Nice Cream. [-50G]
 	 * Papyrus schützt dich vor Undyne. [1x aus dem Gefängnis]
 	 * Du triffst den Fährmann. [Gehe zum nächsten Flughafen]
-	 * Bugerpants gibt dir einen Lebensrat. [Lebe nicht wie ich, ich bin 19 und habe mein ganzes Leben verschwendet.]
+	 * Bugerpants gibt dir einen Lebensrat. [Lebe nicht wie ich, ich bin 19 Jahre alt und habe mein ganzes Leben verschwendet.]
 	 * Du triffst das Goner Kid, du bist star vor schock. [Du kannst dich eine Runde nicht bewegen]
 	 * Sans ruft an, du sollst zum nächsten Wachposten. [Bewege dich zum nächsten Posten von Sans]
 	 * Papyrus hat für dich gekocht, du lehnst ab es zu essen. [Du fühlst dich schlecht]
-	 * 
-	 * 
+	 * Papyrus hat für dich gekocht. [Du isst gefrorene Nudeln]
+	 * Sans lädt dich zu Grillby's ein. [Gehe zu Grillby's]
+	 *
 	 * 
 	 * 
 	 * 
