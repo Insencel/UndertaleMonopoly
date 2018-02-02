602f1da25530d86866b436f75f038eb9428deb51
Kaan <kakirmizi@tsn.at>
2018-02-02 11:56:18 +0100

    Würfel dingens

--- UndertaleMonopoly/src/application/spiel/Würfel.java
+++ UndertaleMonopoly/src/application/spiel/Würfel.java
@@ -1,12 +1,20 @@
 package application.spiel;
 
+import java.util.Random;
+
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class Würfel {
-	private Image[] würfelseiten; //hat nur sechs seiten!
+	private ImageView[] würfelseiten; //hat nur sechs seiten!
+	
 	
 	public int würfeln()
 	{
-		return 0;
+		 Random randomGenerator = new Random();
+		    int randomInt = randomGenerator.nextInt(6)+1;
+			return randomInt;
 	}
+	
+	
 }
