5f58ed0bb5cf8864f62078843ff36e975a9b5105
User <User@SIMONA-PC>
2018-02-09 11:05:57 +0100

    reperatur2

--- UndertaleMonopoly/src/application/eventkarten/eventkarten.java
+++ UndertaleMonopoly/src/application/eventkarten/eventkarten.java
@@ -2,4 +2,34 @@ package application.eventkarten;
 
 public class eventkarten {
 
+	/*
+	 * Monsterkid schenkt dir 50G. [+50G]
+	 * Sans verkauft dir einen Hotdog. [-20G]
+	 * Papyrus fängt dich. [Ins Gefängnis]
+	 * Du bezahlst Temmies College. [-200G]
+	 * Du kaufst Nice Cream. [-50G]
+	 * Papyrus schützt dich vor Undyne. [1x aus dem Gefängnis] (vllt. Du erfährst, dass Papyrus die Tür manchmal nicht abschließt)
+	 * Du triffst den Fährmann. [Gehe zum nächsten Flughafen]
+	 * Bugerpants gibt dir einen Lebensrat. [Lebe nicht wie ich, ich bin 19 Jahre alt und habe mein ganzes Leben verschwendet.]
+	 * Du triffst das Goner Kid, du bist starr vor Schock. [Du kannst dich eine Runde nicht bewegen]
+	 * Sans ruft an, du sollst zum nächsten Wachposten. [Bewege dich zum nächsten Posten von Sans]
+	 * Papyrus hat für dich gekocht, du lehnst ab es zu essen. [Du fühlst dich schlecht]
+	 * Papyrus hat für dich gekocht. [Du isst gefrorene Nudeln]
+	 * Sans lädt dich zu Grillby's ein. [Gehe zu Grillby's]
+	 * 👎︎♓︎♏︎⬧︎ ♓︎⬧︎⧫︎ ♓︎⬧︎⧫︎ ♏︎♓︎■︎ ❄︎♏︎⌧︎⧫︎ ♎︎♏︎■︎ ♎︎◆︎ ■︎♓︎♍︎♒︎⧫︎ ●︎♏︎⬧︎♏︎■︎ &︎♋︎■︎■︎⬧︎⧫︎. ☯︎✋︎■︎ ♎︎♏︎❒︎ ☪︎◆︎&︎◆︎■︎♐︎⧫︎ ♓︎⬧︎⧫︎ ♋︎●︎●︎♏︎⬧︎ ❖︎♏︎❒︎♍︎♒︎❒︎□︎❍︎⧫︎📪︎ 📄︎🗐︎☸︎ [+100G bis -100G Zufällig]
+	 * Du setzt deinen Spielstand zurück. [Gehe zu "RESET" ohne Gold zu bekommen]
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
 }
