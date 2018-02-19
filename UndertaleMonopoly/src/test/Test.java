27e4985bc828ede2ff4d18bcef502890af6c8cee
Kaan <kakirmizi@tsn.at>
2018-02-19 10:39:43 +0100

    Felder

---
+++ UndertaleMonopoly/src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+import application.spiel.Würfel;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Würfel w = new Würfel();
+		for(int i = 0; i<2; i++)
+		{
+			System.out.println(w.würfeln());
+		}
+	}
+
+	
+}
